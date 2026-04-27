2500c64ca2bc186fa7b98d2f5c55ef3857bb7f8e
Sania Tabassum <saniatabassum1904@gmail.com>
2026-04-27 14:53:17 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -11,7 +11,7 @@ from sklearn.metrics import confusion_matrix, precision_score, f1_score, recall_
 sns.set(style='white')
 
 # Load Data
-dataset = pd.read_csv(r"C:\Users\DELL\OneDrive\Desktop\N_Batch -- 7.00PM -- JAN26\N_Batch -- 7.00PM -- JAN26\5. Nov\5th- CICD Pipeline, Web Scrapping\cicd workflows\iris.csv")
+dataset = pd.read_csv("iris.csv")
 
 # Feature names (Ensure no extra spaces or parentheses)
 dataset.columns = [colname.strip(' (cm)').replace(" ", "_") for colname in dataset.columns.tolist()]
